a47c80864d771d213aea44dae49ea78510255d31
Raahul Nair <raahulnair@Raahuls-MacBook-Pro.local>
2024-10-02 18:12:57 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/mproj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mproj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FadingTextAnimation(),
+    );
+  }
+}
+
+class FadingTextAnimation extends StatefulWidget {
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
+}
+
+class _FadingTextAnimationState extends State<FadingTextAnimation> {
+  bool _isVisible = true;
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Fading Text Animation'),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: toggleVisibility,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: const Duration(seconds: 1),
+            curve: Curves.easeInOut,
+            child: const Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: const Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
